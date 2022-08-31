caa7dd35eacc8b32ed2defd4b569c605635e1216
Ousmane KANTE <ousmane.kante@almaviacx.com>
2022-08-31 12:23:28 +0200

    ibexa-4

--- src/bundle/DependencyInjection/EzPlatformFormBuilderMultiStepFormsExtension.php
+++ src/bundle/DependencyInjection/EzPlatformFormBuilderMultiStepFormsExtension.php
@@ -56,6 +56,6 @@ class EzPlatformFormBuilderMultiStepFormsExtension extends Extension implements 
             __DIR__ . '/../Resources/config/ez_field_templates.yml'
         );
 
-        $container->prependExtensionConfig('ezplatform', $config);
+        $container->prependExtensionConfig('ibexa', $config);
     }
 }
